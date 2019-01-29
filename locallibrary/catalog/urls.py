e4d50c6db03a6e791d2e43559e9b194cc8acad12
Tianny Lu <tiannyylu@gmail.com>
2019-01-29 03:07:48 +0000

    Detail tutorial complete

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),    # New line as per tutorial
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 
 # New lines below to serve static files in debug mode
