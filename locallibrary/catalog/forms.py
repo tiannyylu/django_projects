0fbb9fde1885f4ee1b09b4850f625b64aeb6b8c0
Tianny Lu <tiannyylu@gmail.com>
2019-02-12 20:05:26 +0000

    Adding the first form

---
+++ locallibrary/catalog/forms.py
@@ -0,0 +1,15 @@
+import datetime
+
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+class RenewBookForm(forms.Form):
+    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+
+    def clean_renewal_date(self):
+        data = self.cleaned_data['renewal_date']
+
+        # Check if a date is not in the past.
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalid date - renewal in past'))
--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -29,3 +29,13 @@ if settings.DEBUG:
 urlpatterns += [
     path('borrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
