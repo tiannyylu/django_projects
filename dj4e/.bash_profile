f59866c3adca8b4fb28aafb501ffa9dabb802dac
Tianny Lu <tiannyylu@gmail.com>
2019-02-19 23:10:52 -0500

    autos completed

---
+++ dj4e/.bash_profile
@@ -0,0 +1,4 @@
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=<the python3 path you copied before>
+export PROJECT_HOME=$HOME/Devel
+source <the virtualenv path you copied before>
--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name = 'home'),
+    path('hello/', TemplateView.as_view(template_name='home.html'), name = 'home'),
     # path('helloworld', views.helloworld) ,
     path('', TemplateView.as_view(template_name='main.html')),
 ]
